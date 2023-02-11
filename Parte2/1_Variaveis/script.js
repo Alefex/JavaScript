cbd701384c5e5ff1c0b9bad2398af4f2cbb10269
Alefe Tito <alefeptito@gmail.com>
2023-02-11 20:15:36 -0300

    .

---
+++ Parte2/1_Variaveis/script.js
@@ -0,0 +1,16 @@
+let nome = "Kotarou";
+
+console.log(nome);
+console.log(`O nome deste Personagem é ${nome}`);
+
+nome = "Shishi";
+
+console.log(nome);
+
+let vinte =20; trinta =30;
+
+console.log(vinte * trinta);
+
+vinte =2; trinta =3;
+
+console.log(vinte * trinta);
---
+++ Parte2/2_TVar/script.js
@@ -0,0 +1,9 @@
+var nome = "Naruto";
+const nomed = "Aquele que sabe de tudo";
+const ip = "170.0.12.0" ;
+
+console.log(nome);
+
+console.log(nomed);
+
+console.log(ip);
