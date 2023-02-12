94abd4a3b271733b99d41108c2a209f3d4d03d20
Alefe Tito <alefeptito@gmail.com>
2023-02-12 16:24:27 -0300

    .

---
+++ Parte2/3_convencao/script.js
@@ -0,0 +1,16 @@
+// let 3nome = "alan";
+let nomeDELA = "Yugo";
+
+let NAMAEW = "Rury";
+
+let namae3= "Carla";
+
+let NamaeMata = "Kokoro";
+
+let _namae = "Lind";
+
+let $namae = "Rousseau";
+
+// let @$namae = "Corno";
+
+// let !namaew = "Ixxpertinho"
