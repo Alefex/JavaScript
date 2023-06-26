be05e4449de1d05e831d0f9cc47092ce727beecb
Alefe Tito <alefeptito@gmail.com>
2023-06-26 16:19:08 -0300

    node.js project

---
+++ parte_12_nodeproject/db/connection.js
@@ -0,0 +1,9 @@
+//Aplicação para aplicativos relacionais com node/
+const Sequelize = require ('sequelize');
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db/app.db'
+});
+
+module.exports = sequelize
